2aaa02738837b21097f54c958b8f0eb3980e47aa
dantehemerson <dantehemerson@gmail.com>
2018-09-26 00:50:51 -0500

    Logger function

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,22 @@ const signUp = (req, res) => {
 }
 
 const signIn = (req, res) => {
+	User.find({ 
+		email: req.body.email
+	}, (err, user) => {
+		if(err) {
+			return res.status(500).send('Error user not found ' + err)
+		}
+		if(!user) {
+			return res.status(404).send('The user not exists')
+		}
 
+		req.user = user
+		res.status(200).send({
+			message: 'You are logged',
+			token: service.createToken(user)
+		})
+	})
 }
 
 module.exports = {
