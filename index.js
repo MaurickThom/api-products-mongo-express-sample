598f3bae06cd05ed836ae8a213939fa0f2e64eb7
dantehemerson <dantehemerson@gmail.com>
2018-09-25 23:05:52 -0500

    Add config file

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
+
 const app = require('./app')
+const config = require('./config')
 
-const port = process.env.PORT || 3000
-
-mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
 	if(err) {
 		console.log(`Error Establishing Database Connection ${err}`)
 	} 
 	console.log('Established Database Connection')
 
-	app.listen(port, () => {
-		console.log(`Running on http://localhost:${port}`)
+	app.listen(config.port, () => {
+		console.log(`Running on http://localhost:${config.port}`)
 	})
 })
