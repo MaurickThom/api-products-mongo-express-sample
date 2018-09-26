fe954c755d8d13ec5c4dddeb460a0e0423e20ea6
dantehemerson <dantehemerson@gmail.com>
2018-09-25 23:44:53 -0500

    Create authentication controller

---
+++ controllers/auth.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const User = require('../models/user')
+
+const signUp = (req, res) => {
+	const user = new User({
+		email: req.body.email,
+		displayName: req.body.displayName,
+
+	})
+
+	user.save((err) => {
+		if(err) {
+			return res.status(500).send('Error creating user')
+		}
+		return res.status(200).json({
+			token: service.createToken(user)
+		})
+	})
+}
+
+const signIn = (req, res) => {
+
+}
+
+module.exports = {
+	signIn,
+	signUp
+}
