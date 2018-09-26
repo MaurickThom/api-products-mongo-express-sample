096994323858194ea99ede54683ba58833853669
dantehemerson <dantehemerson@gmail.com>
2018-09-26 00:29:07 -0500

    Add middleware of authentication in private routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const api = express.Router()
 
+const isAuth = require('../middlewares/auth')
 const ProductController = require('../controllers/product')
 
 api.get('/product', ProductController.getProducts)
@@ -8,5 +9,9 @@ api.get('/product/:productId', ProductController.getProduct)
 api.post('/product', ProductController.saveProduct)
 api.put('/product/:productId', ProductController.updateProduct)
 api.delete('/product/:productId', ProductController.deleteProduct)
+
+api.get('/private', isAuth, (req, res) => {
+	req.status(200).send('Welcome to private page, You have access')
+})
 
 module.exports = api
